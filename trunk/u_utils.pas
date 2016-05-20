b941a4c60d2ec2d3ede3476ead1585409b1612dc
pchev <pchev@2970548d-3a2c-4a7d-9b02-aac910f5c8e8>
2016-05-20 15:37:54 +0000

    Set version 0.5.0

--- trunk/u_utils.pas
+++ trunk/u_utils.pas
@@ -36,8 +36,8 @@ uses
      Math, SysUtils, Forms, Controls, StdCtrls, Graphics;
 
 const
-  starter_version='Version beta 0.4.0';
-  starterver = '0.4.0';
+  starter_version='Version beta 0.5.0';
+  starterver = '0.5.0';
   blank=' ';
   CR = #$0d;
   LF = #$0a;
