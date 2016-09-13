965d8bb6452546681a776752a55883a6b0ce615c
Patrick Chevalley <pch@ap-i.net>
2016-09-13 11:39:45 +0000

    Set version 0.7.2

--- u_utils.pas
+++ u_utils.pas
@@ -36,8 +36,8 @@ uses
      Math, SysUtils, Forms, Controls, StdCtrls, Graphics;
 
 const
-  starter_version='Version beta 0.7.1';
-  starterver = '0.7.1';
+  starter_version='Version beta 0.7.2';
+  starterver = '0.7.2';
   blank=' ';
   CR = #$0d;
   LF = #$0a;
