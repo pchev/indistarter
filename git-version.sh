250351efcfbfc8e4ea4e48769420504d5e180566
Patrick Chevalley <pch@ap-i.net>
2018-03-04 18:18:27 +0100

    do not show last tag

--- git-version.sh
+++ git-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vcount=$(git rev-list --count --first-parent HEAD)
-vnum=$(git describe --always HEAD)
+vnum=$(git log -n 1 --pretty=format:"%h")
 
 echo "// git revision" > revision.inc
 echo "const RevisionStr = '$vcount-$vnum';" >> revision.inc
