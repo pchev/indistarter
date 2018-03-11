8f07f5489858f8406ec40765b0e4e60061fbab12
Mattia Verga <mattia.verga@tiscali.it>
2018-03-11 10:08:10 +0100

    Move appdata files to metainfo directory

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ echo Install indistarter to $destdir
 install -m 755 -d $destdir
 install -m 755 -d $destdir/bin
 install -m 755 -d $destdir/share
-install -m 755 -d $destdir/share/appdata
 install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/indistarter
@@ -22,12 +21,13 @@ install -m 755 -d $destdir/share/icons
 install -m 755 -d $destdir/share/icons/hicolor
 install -m 755 -d $destdir/share/icons/hicolor/48x48
 install -m 755 -d $destdir/share/icons/hicolor/48x48/apps
+install -m 755 -d $destdir/share/metainfo
 
 install -v -m 755 -s indistarter  $destdir/bin/indistarter
 install -v -m 755 -s indigui  $destdir/bin/indigui
 install -v -m 644 system_integration/Linux/share/applications/indistarter.desktop $destdir/share/applications/indistarter.desktop
 install -v -m 644 system_integration/Linux/share/applications/indigui.desktop $destdir/share/applications/indigui.desktop
-install -v -m 644 system_integration/Linux/share/appdata/indistarter.appdata.xml $destdir/share/appdata/indistarter.appdata.xml
+install -v -m 644 system_integration/Linux/share/metainfo/indistarter.appdata.xml $destdir/share/metainfo/indistarter.appdata.xml
 install -v -m 644 system_integration/Linux/share/doc/indistarter/changelog $destdir/share/doc/indistarter/changelog
 install -v -m 644 system_integration/Linux/share/doc/indistarter/copyright $destdir/share/doc/indistarter/copyright
 install -v -m 644 doc/indistarter.pdf $destdir/share/doc/indistarter/indistarter.pdf
