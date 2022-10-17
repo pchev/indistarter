f3b95c7531f646571bf23162b9fa84da35fe2c4b
Patrick Chevalley <pch@ap-i.net>
2022-10-17 20:36:37 +0200

    List of most recent host and port

--- indigui_connect.pas
+++ indigui_connect.pas
@@ -4,7 +4,7 @@ unit indigui_connect;
 
 interface
 
-uses pu_indigui, u_utils,
+uses pu_indigui, u_utils, XMLConf, LazFileUtils, LCLType,
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, Menus;
 
 type
@@ -18,15 +18,24 @@ type
     Label3: TLabel;
     MainMenu1: TMainMenu;
     MenuHelp: TMenuItem;
-    ServerHost: TEdit;
-    ServerPort: TEdit;
+    ServerHost: TComboBox;
+    ServerPort: TComboBox;
     Timer1: TTimer;
     procedure BtnConnectClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure MenuHelpClick(Sender: TObject);
+    procedure ServerHostKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure ServerPortKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure Timer1Timer(Sender: TObject);
   private
     autoconnect: boolean;
+    config: TXMLConfig;
+    MaxMRU: integer;
+    ConfigDir,configfile: string;
+    procedure LoadConfig;
+    procedure SaveConfig;
+    procedure AddMRU(cb: TComboBox; value: string);
   public
     procedure Init;
   end;
@@ -43,6 +52,12 @@ implementation
 
 procedure Tf_connect.FormCreate(Sender: TObject);
 begin
+  MaxMRU:=10;
+  config:=TXMLConfig.Create(self);
+  ConfigDir:=GetAppConfigDirUTF8(false,true);
+  configfile:='indigui.conf';
+  config.Filename:=slash(ConfigDir)+configfile;
+  LoadConfig;
   autoconnect:=true;
   if Application.HasOption('h', 'host') then begin
     ServerHost.Text:=Application.GetOptionValue('h', 'host');
@@ -53,6 +68,43 @@ begin
   if Application.HasOption('n', 'noconnect') then begin
     autoconnect:=false;
   end;
+end;
+
+procedure Tf_connect.LoadConfig;
+var i,n: integer;
+begin
+  ServerHost.Clear;
+  n:=config.GetValue('/Host/Num',0);
+  for i:=1 to n do begin
+    ServerHost.Items.Add(config.GetValue('/Host/Host'+inttostr(i),''));
+  end;
+  if ServerHost.Items.Count=0 then
+    ServerHost.Items.Add('localhost');
+  ServerHost.ItemIndex:=0;
+  ServerPort.Clear;
+  n:=config.GetValue('/Port/Num',0);
+  for i:=1 to n do begin
+    ServerPort.Items.Add(config.GetValue('/Port/Port'+inttostr(i),''));
+  end;
+  if ServerPort.Items.Count=0 then
+    ServerPort.Items.Add('7624');
+  ServerPort.ItemIndex:=0;
+end;
+
+procedure Tf_connect.SaveConfig;
+var i,n: integer;
+begin
+  n:=ServerHost.Items.Count;
+  config.SetValue('/Host/Num',n);
+  for i:=0 to n-1 do begin
+    config.SetValue('/Host/Host'+inttostr(i+1),ServerHost.Items[i]);
+  end;
+  n:=ServerPort.Items.Count;
+  config.SetValue('/Port/Num',n);
+  for i:=0 to n-1 do begin
+    config.SetValue('/Port/Port'+inttostr(i+1),ServerPort.Items[i]);
+  end;
+  config.Flush;
 end;
 
 procedure Tf_connect.Init;
@@ -67,12 +119,31 @@ begin
   BtnConnectClick(Sender);
 end;
 
-procedure Tf_connect.BtnConnectClick(Sender: TObject);
+procedure Tf_connect.AddMRU(cb: TComboBox; value: string);
+var i: integer;
 begin
+  i := cb.Items.IndexOf(value);
+  if (i < 0) and (cb.Items.Count >= MaxMRU) then
+    i := MaxMRU - 1;
+  if i >= 0 then
+    cb.Items.Delete(i);
+  cb.Items.Insert(0, value);
+  cb.ItemIndex := 0;
+end;
+
+procedure Tf_connect.BtnConnectClick(Sender: TObject);
+var h,p: string;
+begin
+  h:=ServerHost.Text;
+  if trim(h)='' then exit;
+  AddMRU(ServerHost,h);
+  p:=ServerPort.Text;
+  if trim(p)='' then exit;
+  AddMRU(ServerPort,p);
   f_indigui:=Tf_indigui.Create(self);
   f_indigui.Caption:='INDI Client '+starter_version+'-'+RevisionStr;
-  f_indigui.IndiServer:=ServerHost.Text;
-  f_indigui.IndiPort:=ServerPort.Text;
+  f_indigui.IndiServer:=h;
+  f_indigui.IndiPort:=p;
   hide;
   f_indigui.ShowModal;
   if not f_indigui.ConnectedServer then
@@ -81,6 +152,11 @@ begin
     if f_indigui.DisconnectedServer then ShowMessage('Disconnected from server');
     close;
   end;
+end;
+
+procedure Tf_connect.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  SaveConfig;
 end;
 
 procedure Tf_connect.MenuHelpClick(Sender: TObject);
@@ -104,6 +180,16 @@ aboutmsg:=aboutmsg+'modify it under the terms of the GNU General Public License'
 aboutmsg:=aboutmsg+'as published by the Free Software Foundation; either version 3'+crlf;
 aboutmsg:=aboutmsg+'of the License, or (at your option) any later version.'+crlf;
 ShowMessage(aboutmsg);
+end;
+
+procedure Tf_connect.ServerHostKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if key=VK_RETURN then BtnConnectClick(Sender);
+end;
+
+procedure Tf_connect.ServerPortKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if key=VK_RETURN then BtnConnectClick(Sender);
 end;
 
 
